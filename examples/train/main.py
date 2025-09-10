4aec28af3d5d649ecb1d15f7cc7cef2ebd3f3ff3
6GOD <55304045+6ixGODD@users.noreply.github.com>
2025-09-11 00:09:33 +0800

    feat: add configuration files for training and testing, and update main entry point

--- examples/train/main.py
+++ examples/train/main.py
@@ -1,1 +1,10 @@
 from __future__ import annotations
+
+from visflow.resources.configs import TrainConfig
+from visflow.pipelines.train import TrainPipeline
+
+
+pipeline = TrainPipeline(TrainConfig.from_yaml('.train.yml'))
+
+if __name__ == '__main__':
+    pipeline()
